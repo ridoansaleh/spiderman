dff458c9dee704100bd977d6cf45efd87cc7418f
ridoansaleh <ridoansalehnasution98@gmail.com>
2019-07-24 16:43:07 +0700

    improvement 1

--- src/App.js
+++ src/App.js
@@ -3,20 +3,17 @@ import { connect } from 'react-redux';
 import TodoInput from './components/TodoInput';
 import TodoUpdate from './components/TodoUpdate';
 import TodoList from './components/TodoList';
+import './css/MainStyle.css';
 import { fetchAllTodos, saveTask } from './redux/creators';
 
 class App extends Component {
-  state = {
-    todos: [],
-  };
-
   componentDidMount() {
     this.props.fetchAllTodos();
   }
 
   render() {
     return (
-      <div style={{ width: '30%', marginLeft: '35%' }}>
+      <div className="todo-container">
         <TodoInput todos={this.props.todos.data} saveTask={this.props.saveTask} />
         <TodoUpdate />
         <TodoList todos={this.props.todos.data} />
